ebc6e1cc170c44f40d62b2f14bc092779d100557
Crowdbotics <team@crowdbotics.com>
2020-10-21 20:40:29 +0000

    Update models

--- backend/manage.py
+++ backend/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smb_community_21802.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "smb_community_21802.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -17,5 +17,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- backend/smb_community_21802/wsgi.py
+++ backend/smb_community_21802/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smb_community_21802.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "smb_community_21802.settings")
 
 application = get_wsgi_application()
